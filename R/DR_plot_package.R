9ba6c518eef2457ef0f5a3a236ed6253519371a9
Alobondo <45467026+Alobondo@users.noreply.github.com>
2021-11-29 11:05:51 -0300

    Add files via upload

--- R/DR_plot_package.R
+++ R/DR_plot_package.R
@@ -12,11 +12,11 @@ DR_plot <- function(dr){
   aux$Mean <- as.numeric(aux$Mean)
 
   ggplot(data_long_month, aes(x=as.factor(Month), y=Value, group=year, color=year)) +
-    geom_line(size = 1) +
+    geom_line(size = 1, alpha = 0.75) +
     scale_x_discrete(labels=month.abb[seq(1,12)], guide = guide_axis(angle = 90)) +
-    geom_line(data = aux,aes(x=factor(Month, levels = month.abb),y=Mean,fill="mean"),size = 1.5, color = "red", linetype = "dashed", group =1,show.legend = TRUE) +
+    geom_line(data = aux,aes(x=factor(Month, levels = month.abb),y=Mean,fill="mean"),size = 1.75, color = "red", linetype = "dashed", group =1,show.legend = TRUE) +
     scale_fill_manual("line", values=c(1),guide=guide_legend(override.aes = list(colour=c("red")))) +
-    ggtitle("Monthly values filled with Rational Deductive Method") +
+    ggtitle("Monthly values filled with Deductive Rational Method") +
     xlab("Month") + ylab("Value") +
     theme_bw()
 
