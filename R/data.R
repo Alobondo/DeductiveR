75ad1151a5c7f42870c2e34243298654604cdb3c
Alobondo <45467026+Alobondo@users.noreply.github.com>
2021-12-07 14:11:20 -0300

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Monthly flow data from Estero Culebron DGA station
+#'
+#' A dataset containing dates and monthly flow values.
+#'
+#' @format A data frame with 480 rows and 5 variables:
+#' \describe{
+#'   \item{date}{dates}
+#'   \item{year}{years}
+#'   \item{month}{months}
+#'   \item{day}{days}
+#'   \item{4400001}{flows, in cubic meters per second}
+#' }
+#' @source \url{https://camels.cr2.cl/}
+"data"
